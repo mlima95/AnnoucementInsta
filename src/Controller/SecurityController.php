5467b58a7915ccfcefcfebf01ec5170280a75b3a
mlima95 <milan.lima@hotmail.fr>
2019-09-27 17:27:24 +0200

    add api

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -33,6 +33,7 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
+        $this->redirectToRoute('home');
         throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
     }
 }
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -10,6 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
  */
+
 class Product
 {
     /**
